63b68eb85e23b69411384bfcf8fb128448c09f4d
Polina <polyamsk@yandex.ru>
2022-11-15 15:28:58 +0400

    Add task2

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b) Console.WriteLine($"Максимальное число = {a} ");
+else Console.WriteLine($"Максимальное число = {b} ");
