3fd4b5ce6d93dd5961ab82addbd89da6c1ea8ded
Polina <polyamsk@yandex.ru>
2022-11-27 18:18:41 +0400

    Add task 30 from seminar

---
+++ task 30/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программ, которая выводит мссив из 8 элементгов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+int[] array = new int [8];
+
+void RandomArray(int[] array){
+    Random rnd = new Random();
+    for(int i = 0; i < array.Length; i++){
+        array[i] = rnd.Next(0,2);
+    }
+}
+
+void PrintArray(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]}");
+    }
+}
+RandomArray(array);
+
+PrintArray(array);
