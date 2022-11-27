8a91e5a067c79507447db4a075484e0bef15bca7
Polina <polyamsk@yandex.ru>
2022-11-27 18:18:13 +0400

    Add task 28 from seminar

---
+++ task 28/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход число N
+и выдает произведение чисел от 1 до N.
+4 -> 24
+5 -> 120  */
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultNumbers(int num){
+    int mult = 1;
+    for (int i = 1; i <= num; i++){
+        mult = mult*i;
+    }
+    return mult;
+}
+
+if (number > 0){
+    int multNumbers = MultNumbers(number);
+    Console.WriteLine($" {number} -> {multNumbers}");
+}
+
+else Console.WriteLine("Требуется ввести натуральное число.");
