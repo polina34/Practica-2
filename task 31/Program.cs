b23cd8d6928cdd13678004970b23876f333071ae
Polina <polyamsk@yandex.ru>
2022-12-01 23:42:43 +0400

    Add task 31

---
+++ task 31/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 31: 
+// 1. Задайте массив из 12 элементов, 
+// 2. заполненный случайными числами из промежутка [-9, 9]. 
+// 3. Найдите сумму отрицательных и положительных элементов массива.
+// 4. Вывод результата
+
+// [3,9,-8,1,0,-7,2,-1,8,-3,-1,6]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    var array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    var array = new double[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        double rndNum = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(rndNum, 2);
+
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void PrintArrayDouble (double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] GetSumPositiveNegativElem (int[] array)
+{
+    int sumNegative = default;
+    int sumPositive = default;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sumNegative += array[i];
+        else sumPositive += array[i];
+    }
+
+    return new int[] {sumNegative, sumPositive};
+}
+
+// int[] arr = CreateArrayRndInt(12, -9, 9);
+// PrintArray(arr);
+// int[] result = GetSumPositiveNegativElem(arr);
+
+// Console.WriteLine($"Сумма положительных чисел = {result[1]}");
+// Console.WriteLine($"Сумма отрицательных чисел = {result[0]}");
+
+double[] arr1 = CreateArrayRndDouble(12, -9, 9);
+PrintArrayDouble(arr1);
