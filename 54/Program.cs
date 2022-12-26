2fe316ffa022f1a13d94b4d9cce171779f10167d
Polina <polyamsk@yandex.ru>
2022-12-27 00:54:55 +0400

    Added hometask

---
+++ 54/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по
+// убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] RandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string sep1, string sep2)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{sep1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine($"{sep2}");
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+int stringsize = 0;
+int columsize = 0;
+
+while (stringsize <= 0 || columsize <= 0)
+{
+    Console.WriteLine($"Введите количество сторок");
+    stringsize = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите количество столбцов");
+    columsize = Convert.ToInt32(Console.ReadLine());
+    if (stringsize <= 0 || columsize <= 0)
+        Console.WriteLine("Введены неверные данные");
+}
+
+
+int[,] array2D = RandomMatrix(stringsize, columsize, 0, 9);
+PrintMatrix(array2D, "|", "|");
+SortToLower(array2D);
+Console.WriteLine();
+PrintMatrix(array2D, "|", "|");
---
+++ 56/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] RandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix, string sep1, string sep2)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"{sep1}");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}|");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine($"{sep2}");
+    }
+}
+
+int NumberRowMinSumElements(int[,] matrix)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) sumRow += matrix[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    return minSumRow;
+}
+
+int stringsize = 0;
+int columsize = 0;
+
+while (stringsize <= 0 || columsize <= 0)
+{
+    Console.WriteLine($"Введите количество сторок");
+    stringsize = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите количество столбцов");
+    columsize = Convert.ToInt32(Console.ReadLine());
+    if (stringsize <= 0 || columsize <= 0)
+        Console.WriteLine("Введены неверные данные");
+}
+int[,] array2D = RandomMatrix(stringsize, columsize, 0, 9);
+PrintMatrix(array2D, "|", "|");
+Console.WriteLine();
+Console.WriteLine($"{NumberRowMinSumElements(array2D) + 1} строка");
